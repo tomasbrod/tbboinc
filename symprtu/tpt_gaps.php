a145fb753ce995681623721258505acfddd99c44
Tomáš Brada <tomasbrod@azet.sk>
2020-03-21 21:16:08 +0100

    tpt_gaps.php

---
+++ symprtu/tpt_gaps.php
@@ -0,0 +1,52 @@
+<?php
+/* spt/list.php - list found prime tuples from database */
+require_once("../inc/util.inc");
+require_once("../inc/boinc_db.inc");
+
+function show() {
+  $t= get_int('t');
+  $f= get_int('f');
+  header("Content-type: text/plain");
+  $db = BoincDb::get();
+  $clausule=" k=0 ";
+  if($t>0)
+    $clausule=" k>0 ";
+  $set = $db->do_query("select start, d, k, ofs, resid from spt_gap where $clausule order by start");
+  echo "# Copyright Tomas Brada, ask on forum about reuse or citation.\n";
+  echo "# tp_gaps where $clausule\n";
+  $ix=1;
+  while($row = $set->fetch_object('stdClass')) {
+    if($f==1) {
+      echo "$ix {$row->start}\n";
+    } else
+    if($f==2) {
+      echo "$ix ".($row->d+2)."\n";
+    } else
+    if($f==3) {
+      echo "{$row->start}: 0 2";
+      $a=2;
+      foreach($row->ofs as $d) {
+	$a1+=$d;
+	$a=$a1+2;
+	echo " $a1 $a"
+      }
+      echo " [$ix {$row->resid}]\n";
+    } else
+    if($f==4) {
+      echo "$ix todo A329164\n";
+    } else
+    if($f==5) {
+      echo "$ix todo A329165\n";
+    } else
+    {
+      echo "$ix {$row->start} {$row->ofs}\n";
+    }
+    $astr=convert_tuple($row->ofs,$row->k);
+    echo "{$row->start}: $astr\n";
+    if($row->id>$lastid) $lastid=$row->id; $rcount++;
+  }
+  echo "# last = $lastid # count = $rcount\n";
+  $set->free();
+}
+
+show();
