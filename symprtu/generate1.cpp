f8692ea4cc6feea4ee7bcfd6aa199f4e3ee2af5f
Tomáš Brada <tomasbrod@azet.sk>
2020-06-11 14:22:11 +0200

    batch 72

--- symprtu/generate1.cpp
+++ symprtu/generate1.cpp
@@ -186,11 +186,11 @@ int main(int argc, char** argv) {
 	if(boinc_db.start_transaction())
 		exit(4);
 
-	uint64_t start= 500001145000000000;
-	uint64_t   end= 600000000000000000;
-	uint64_t  step=      1965000000000;
-	unsigned maxcnt = 99000;
-	int batch = 71;
+	uint64_t start= 600001960000000000;
+	uint64_t   end= 900000000000000000;
+	uint64_t  step=      1950000000000;
+	unsigned maxcnt = 64000;
+	int batch = 72;
 	uint64_t next = start;
 	unsigned long count = 0;
 	while(1) {
@@ -204,7 +204,7 @@ int main(int argc, char** argv) {
 		submit_wu_in(curr, next, batch);
 		count++;
 	}
-	post_batch_msg(batch,start,next,count,"spt-sh","From 5e17.");
+	post_batch_msg(batch,start,next,count,"spt-ut","From 6e17.");
 	cerr<<"Count: "<<count<<endl;
 	cerr<<"First: "<<start<<endl;
 	cerr<<"Next : "<<next<<endl;
