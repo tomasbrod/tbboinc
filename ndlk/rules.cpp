ae43549b7875ee34f25c36f595a107032cdfdcd7
Tomáš Brada <tomasbrod@azet.sk>
2020-08-18 12:10:39 +0200

    ndlk: messing with rules

--- ndlk/rules.cpp
+++ ndlk/rules.cpp
@@ -16,9 +16,54 @@ using std::endl;
 #define DLX_QUIET
 
 #include "dlk_util.cpp"
-#include "kanonizer_b.cpp"
+#include "kanonizer_v.cpp"
 
-Kanonizer kanonizer;
+KanonizerV kanonizer;
+
+void fill()
+{
+	const unsigned order = 10;
+	kanonizer.init_order(order);
+	Square sq(order);
+	std::set<std::vector<unsigned>> ruleset;
+	std::vector<unsigned> rule ( order, order );
+	//natural main diagonal
+	for(unsigned i=0; i<order; ++i)	sq(i,i) = i;
+	std::vector<bool> options(order,false);
+	//for odd-order, fix the middle cell
+	if(order&1) options[order/2] = true;
+	unsigned r=0; // row
+	unsigned v=0; // value
+	while(1) {
+		if(r<order) {
+			for(1; v<order && (options[v] || v==sq(r,r)); ++v) ;
+			if(v<order) {
+				std::cout<<r<<"+"<<v<<endl;
+				sq.anti(r) = v;
+				options[v] = true;
+				r++;
+				if(order&1 && r==order/2) r++;
+				v=0;
+				continue;
+			}
+		} else {
+			std::cout<<sq<<endl;
+			kanonizer.im_find_can(0, &rule[0], sq);
+			for(unsigned i=0; i<order; ++i)
+				std::cout<<rule[i]<<" ";
+			// 0 2 1 4 3 6 5 8 7 9 is wrong, 0 cant be first!!!
+			std::cout<<endl;
+			ruleset.insert(rule);
+		}
+		if(!r) break;
+		r--;
+		if(order&1 && r==order/2) r--;
+		v=sq.anti(r);
+		std::cout<<r<<"-"<<v<<endl;
+		options[v] = false;
+		v++;
+	}
+}
 
 int main(int argc, char* argv[])
 {
@@ -30,39 +75,7 @@ int main(int argc, char* argv[])
 		return 9;
 	}
 	try {
-		std::set<std::vector<unsigned>> ruleset;
-		while(std::cin) {
-			std::string line;
-			std::getline(std::cin,line);
-			if( line!="" && line[0]!=' ' && line[0]!='#' ) {
-				Square sq;
-				sq.Decode(line);
-				if(!sq.width()) throw std::runtime_error("Zero-width square");
-
-				std::set<Square> isotopes;
-				kanonizer.Kanon(sq, &isotopes);
-				std::vector<unsigned> map ( sq.width() );
-				std::vector<unsigned> rule; rule.resize(sq.width(), sq.width());
-				std::vector<unsigned> anti( sq.width() );
-				const unsigned N = sq.width()-1;
-				for(Square izo : isotopes) {
-					for(unsigned i=0; i<sq.width(); ++i)
-						map[izo(i,i)] = i;
-					for(unsigned i=0; i<sq.width(); ++i)
-						anti[i] = map[ izo(i, N-i) ];
-					if(anti<rule)
-						rule=anti;
-				}
-				auto it = ruleset.insert(rule);
-				if(it.second) {
-					for(unsigned i=0; i<N; ++i)
-						std::cout<<rule[i]<<" ";
-					std::cout<<rule[N]<<endl;
-					std::cout.flush();
-				}
-			}
-		}
-		std::cout<<"Rules: "<<ruleset.size()<<endl;
+		fill();
 	}
 	catch( const std::exception& e ) {
 		std::cerr<<"Exception: "<<e.what()<<endl;
