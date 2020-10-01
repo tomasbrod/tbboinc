b3ed7d8ca6a1b61bee46bdc4d70144596a183548
Tomáš Brada <tomasbrod@azet.sk>
2020-10-01 07:59:13 +0200

    schedq feeder simple

---
+++ boinc/sched/feeder_simple.cpp
@@ -0,0 +1,128 @@
+// This file is part of BOINC.
+// http://boinc.berkeley.edu
+// Copyright (C) 2020 University of California
+// SchedQ: Ve.Brod
+//
+// BOINC is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// BOINC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+
+// Simple result scheduler for CPU only.
+
+#include "config.h"
+#ifdef _USING_FCGI_
+#include "boinc_fcgi.h"
+#else
+#include <cstdio>
+#endif
+#include <cassert>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <cmath>
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+#include "backend_lib.h"
+#include "boinc_db.h"
+#include "error_numbers.h"
+#include "filesys.h"
+#include "parse.h"
+#include "str_replace.h"
+#include "str_util.h"
+#include "util.h"
+
+#include "schedq_handle.h"
+
+using std::runtime_error;
+
+struct FEEDER_SIMPLE : SCHED_QUEUE {
+	DB_APP app;
+	FEEDER_SIMPLE(const DB_QUEUE& q)
+		: SCHED_QUEUE(q)
+	{
+		/*
+		 * Parse the param xml to find which app to send (batch?, prio?)
+		 * Cache the application versions maybe?
+		*/
+    char attr_buf[256], app_name[256];
+    MIOFILE mf;
+    mf.init_buf_read(q.args);
+		XML_PARSER xp(&mf);
+		if(
+			xp.get_tag(attr_buf, sizeof(attr_buf))
+			|| xp.match_tag("wu")
+			) throw runtime_error("no start tag");
+		parse_attr(attr_buf, "app", app_name, sizeof(app_name));
+		app.db=q.db;
+		if(app.lookup_name(app_name))
+			throw runtime_error("app not found");
+	}
+	int select_app_version(DB_APP_VERSION &version, SCHEDULER_REPLY& sreply) {
+    char where [MAX_QUERY_LEN] = {0};
+    snprintf(where, sizeof where, "WHERE appid = %lu and deprecated=0 order by version_num desc", app.id);
+    while(version.enumerate(where)) {
+			//let's pretend its done
+		}
+		return 0;
+	}
+	bool assign_result(SCHEDULER_REPLY& sreply, DB_RESULT& result)
+	{
+		return false;
+	}
+	void feed(SCHEDULER_REPLY& sreply)
+	{
+		//again, do nothing
+		/* Find app version for the host. Start transaction.
+		 * Fetch few results from db. FOR UPDATE
+		 * Try to assign them.
+		 * If assigned more than half, repeat query, else commit.
+		 * Last, adjust user quota.
+		*/
+		// very simple version picking - enumerate backwards and pick first one that matches host platform
+		DB_APP_VERSION version (sreply.db);
+		if(select_app_version(version, sreply)) {
+			message(sreply, "low", "No app version for app %s queue %s", app.name, this->name);
+			return;
+		}
+    char where [MAX_QUERY_LEN] = {0};
+		// assert in transaction
+		DB_RESULT result (sreply.db);
+		const unsigned select_limit = 10;
+    snprintf(where, sizeof where,
+			"FOR update skip locked WHERE appid = %lu and server_state=%d limit %d"
+			, app.id
+			, RESULT_SERVER_STATE_UNSENT
+			, select_limit
+		);
+		while(1) {
+			unsigned assigned = 0;
+			while(result.enumerate(where)) {
+				assigned += assign_result(sreply, result);
+				if(schedq_reply_full(sreply))
+					break;
+			}
+			if(assigned < select_limit/2)
+				break;			
+		}
+		
+	}
+};
+
+SCHED_QUEUE* create_feeder_simple(DB_QUEUE& descr) { return new FEEDER_SIMPLE(descr); }
